32f13f97270cbeb20f63367c919d5d228a96ecb7
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-12-11 09:25:27 +0700

    Add global_variables.gd and autoloaded it

---
+++ globals/global_variables.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# focus
+var FOCUS_TIME_IN_MINUTE: int = 25
+var focus_time_pointer: int
+
+# break
+var BREAK_TIME_IN_MINUTE: int = 5
+var break_time_pointer: int
+
+# long break
+var LONG_BREAK_TIME_IN_MINUTE: int = 10
+var long_break_time_pointer: int
+
+var BACKGROUND_FOCUS = Color('bb36448c')
+var BACKGROUND_BREAK = Color('287794ff')
+var BACKGROUND_LONG_BREAK = Color('2877948c')
