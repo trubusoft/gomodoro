2b308cc32f32ebf70882ce1451fda0c639daceeb
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-12-19 11:01:57 +0700

    Update Task to toggle panel expansion based on user click

--- scenes/task.gd
+++ scenes/task.gd
@@ -10,14 +10,17 @@ signal toggle_check(toggled_on: bool)
 @onready var check_box = %CheckBox
 
 
+var is_panel_expanded = false
 var grow_size = 100
 
 
-func toggle_expand_panel(toggled_on: bool):
-	if toggled_on:
+func toggle_expand_panel():
+	if is_panel_expanded == false:
+		is_panel_expanded = true
 		task.custom_minimum_size.y += grow_size
 		panel.size.y += grow_size
-	else:
+	elif is_panel_expanded == true:
+		is_panel_expanded = false
 		task.custom_minimum_size.y -= grow_size
 		panel.size.y -= grow_size
 
@@ -30,6 +33,16 @@ func _process(_delta):
 	pass
 
 
+func is_click(event) -> bool:
+	if event is InputEventMouseButton and event.button_index == 1 and event.pressed == true:
+		return true
+	return false
+
+
 func _on_check_box_toggled(toggled_on):
 	toggle_check.emit(toggled_on)
-	toggle_expand_panel(toggled_on)
+
+
+func _on_panel_gui_input(event):
+	if is_click(event):
+		toggle_expand_panel()
